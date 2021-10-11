74ec1ad42e0c7fe7b18be3a505f16613b7b38871
Dustin Mahle <dmahle20401@gmail.com>
2021-10-11 01:36:47 -0400

    Code Zero Added

---
+++ src/assignment/CodeZero.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class CodeZero {
+
+    public static void main(String[] args) {
+        System.out.println("Print line");
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println();
+        System.out.println("C");
+
+        //Print ints directly, no conversion needed.
+        int value = 10;
+        System.out.println(value);
+        System.out.println(value * 10);
+
+    }
+}
